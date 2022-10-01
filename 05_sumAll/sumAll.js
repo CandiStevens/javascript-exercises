50a5e7226eb1f1a923e08a93e52e98065d1f7704
candi <candistevens28@gmail.com>
2022-10-01 17:27:04 -0600

    finished leap year. was easier than the last couple.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    if (typeof end === 'string' || typeof start === 'string') {
+       return 'ERROR';
+    }
 
+    if (isNaN(start) || isNaN(end)) {
+        return 'ERROR'
+    }
+
+    if (start < 0 || end < 0) {
+       return 'ERROR';
+    }
+
+    if (start > end) {
+     end =[start, start = end][0];
+    }
+    
+    let finalSum = 0;
+    for (let i = start; i < end + 1; i++) {
+        finalSum = finalSum + i;
+    }
+    return finalSum;
+    
 };
-
+console.log(sumAll(5, 3));
 // Do not edit below this line
 module.exports = sumAll;
